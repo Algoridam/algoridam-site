bd221d793cf6197ded2ff3da2fa41b97072a602a
Ademola Adigun <ademolaadigun@Ademolas-MacBook-Air.local>
2024-03-17 23:22:02 +0100

    added hero content

---
+++ components/hero-section.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <>
+            <div className='text-center text-6xl mt-24 mb-4'>
+                Lorem ipsum dolor sit amet <br /> consectetur
+            </div>
+            <div className='text-center mb-4'>
+                Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur
+            </div>
+            {/* Center the Hero dropdown */}
+        </>
+    )
+}
+
+export default HeroSection
