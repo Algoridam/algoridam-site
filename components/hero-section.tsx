06b043a7201b225d12c6d4173c25ff683d9a933d
Kabir Ademola Adigun <kabir@fyxn.nl>
2024-04-12 21:41:17 +0100

    added svgs, added header

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
     return (
         <>
+            <Image className='absolute z-50' src="/bubble 1 1.svg" alt="Vercel Logo" width={250} height={50} layout="intrinsic" />
             <div className='text-center text-6xl mt-24 mb-4'>
                 Lorem ipsum dolor sit amet <br /> consectetur
             </div>
+            <Image className='absolute z-50 left-9' src="/bubble 2 1.svg" alt="Vercel Logo" width={250} height={50} layout="intrinsic" />
             <div className='text-center mb-4'>
                 Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur
             </div>
             {/* Center the Hero dropdown */}
+
         </>
     )
 }
