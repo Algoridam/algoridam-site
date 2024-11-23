3fda3428d8f6abe018394009b3bce07b17af806a
aheedjo <maishanu.ahijo@yahoo.com>
2024-11-23 02:43:35 +0100

    Resolved build erros

--- components/LanguageWrapper.tsx
+++ components/LanguageWrapper.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Head from 'next/head';
 import useLanguageStore from '@/zeustand/languageStore';
 
 interface LanguageWrapperProps {
@@ -11,15 +12,18 @@ const LanguageWrapper: React.FC<LanguageWrapperProps> = ({ children }) => {
   const { language } = useLanguageStore();
 
   return (
-    <html lang={language}>
-      <head>
-        <link rel="icon" href="/algoridam-favicon.svg" />
-      </head>
-      <body className="bg-gradient-custom from-custom-start to-custom-endbg-gradient-custom to-custom-end">
+    <>
+      <Head>
+        <html lang={language} />
+        <link rel="icon" href="/algoridam.svg" />
+      </Head>
+      <body className="bg-gradient-custom from-custom-start to-custom-end">
         {children}
       </body>
-    </html>
+    </>
   );
 };
+
+LanguageWrapper.displayName = 'LanguageWrapper';
 
 export default LanguageWrapper;
