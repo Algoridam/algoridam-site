4c0fcea69a9918daee8c44fb91c2fd3f795b5b69
aheedjo <maishanu.ahijo@yahoo.com>
2025-01-08 02:38:38 +0100

    Add 'Instrument Sans' font and update HTML lang attribute and favicon

--- components/LanguageWrapper.tsx
+++ components/LanguageWrapper.tsx
@@ -3,10 +3,17 @@
 import React, { useEffect } from 'react';
 import Head from 'next/head';
 import useLanguageStore from '@/zeustand/languageStore';
+import { Instrument_Sans } from 'next/font/google';
 
 interface LanguageWrapperProps {
   children: React.ReactNode;
 }
+
+const instrumentSans = Instrument_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
 
 const LanguageWrapper: React.FC<LanguageWrapperProps> = ({ children }) => {
   const { language } = useLanguageStore();
@@ -14,8 +21,8 @@ const LanguageWrapper: React.FC<LanguageWrapperProps> = ({ children }) => {
   return (
     <>
       <Head>
-        <html lang={language} />
-        <link rel="icon" href="/algoridam.svg" />
+        <html lang={language} className={instrumentSans.className} />
+        <link rel="icon" type="image/svg+xml" href="/algoridam.svg" />
       </Head>
       <body className="bg-gradient-custom from-custom-start to-custom-end">
         {children}
